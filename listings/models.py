696c3a290bfb7301a641b5c1ab59475a05538bd9
sk011094 <sinyili2004@gmail.com>
2024-09-04 12:43:08 +0800

    step 8.2:building database and models-install pillow

--- listings/models.py
+++ listings/models.py
@@ -4,7 +4,7 @@ from realtors.models import Realtor
 
 # Create your models here.
 class Listing(models.Model):
-    realtor = models.ForeignKey('Realtor', on_delete=models.DO_NOTHING)
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     street = models.CharField(max_length=200)
--- realtors/models.py
+++ realtors/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 
-class Realtors(models.Model):
+class Realtor(models.Model):
     name = models.CharField(max_length=200)
     photo= models.ImageField(upload_to='photos/%Y/%m/%d/')
     description = models.TextField(blank=True)
