e44378994b9ed539815056acac7fd07fec1c62f0
sk011094 <sinyili2004@gmail.com>
2024-09-04 12:01:05 +0800

    step 8:building database and models

--- bcre/settings.py
+++ bcre/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcre',
+        'USER':'postgres',
+        'PASSWORD': 'sk011094',
+        'HOST': 'localhost'
     }
 }
 
